9a8e0f81195e5d8613dde3f61dced3f1bf104511
ashutosh-maker <ashutoshkarat95@gmail.com>
2021-12-13 14:32:14 +0530

    final commit

--- welcome.java
+++ welcome.java
@@ -2,7 +2,7 @@ class welcome
 {
 	public static void main(string args[])
 	{
-		system.out.println("welcome");
+		system.out.println("welcome ashutosh");
                 system.out.println("welcome again");
 	}
 }
